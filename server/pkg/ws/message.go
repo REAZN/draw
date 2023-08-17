a07bda2945c0f6a9ecf44b67f4605c4995da056c
REAZN <lewis@reazn.me>
2023-08-17 09:29:35 +0100

    server: message res

--- server/pkg/ws/message.go
+++ server/pkg/ws/message.go
@@ -9,15 +9,20 @@ import (
 )
 
 type MessageResponse struct {
-	Event   string `json:"event"`
-	Message string `json:"message"`
+	Event string              `json:"event"`
+	Data  MessageResponseData `json:"data"`
+}
+
+type MessageResponseData struct {
 	Name    string `json:"name"`
+	Message string `json:"message"`
 }
 
 func (r *Room) HandleMessage(c *websocket.Conn, content map[string]interface{}) {
 
 	messageData := &MessageContent{}
 	utils.FillStruct(content, messageData)
+	fmt.Println(messageData)
 
 	for roomId, conns := range r.rooms {
 		for _, conn := range conns {
@@ -25,7 +30,11 @@ func (r *Room) HandleMessage(c *websocket.Conn, content map[string]interface{}) 
 				for _, otherConn := range conns {
 					if otherConn.Conn != c {
 						fmt.Println(roomId, messageData.Message, rand.Int())
-						data, err := json.Marshal(MessageResponse{Event: "message", Message: messageData.Message, Name: conn.Name})
+						data, err := json.Marshal(MessageResponse{Event: "message",
+							Data: MessageResponseData{
+								Message: messageData.Message,
+								Name:    conn.Name,
+							}})
 						err = otherConn.Conn.WriteMessage(websocket.TextMessage, data)
 						if err != nil {
 							return
