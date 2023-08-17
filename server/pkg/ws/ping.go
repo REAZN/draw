f50ae1d06bab07720a82a4bc43b17e68a5c51f23
REAZN <lewis@reazn.me>
2023-08-17 09:29:42 +0100

    server: ping res

--- server/pkg/ws/ping.go
+++ server/pkg/ws/ping.go
@@ -1,12 +1,23 @@
 package ws
 
 import (
+	"encoding/json"
 	"github.com/gofiber/contrib/websocket"
 )
 
+type PingResponse struct {
+	Event string           `json:"event"`
+	Data  PingResponseData `json:"data"`
+}
+
+type PingResponseData struct {
+	Message string `json:"message"`
+}
+
 func HandlePing(c *websocket.Conn) {
 
-	err := c.WriteMessage(websocket.TextMessage, []byte("Pong!"))
+	data, err := json.Marshal(PingResponse{Event: "ping", Data: PingResponseData{Message: "Pong!"}})
+	err = c.WriteMessage(websocket.TextMessage, data)
 	if err != nil {
 		return
 	}
